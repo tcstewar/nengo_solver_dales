78e8522122a53a761c41a248180bf6737ceea680
Terry Stewart <tcstewar@uwaterloo.ca>
2018-08-30 16:38:53 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='nengo_solver_dales',
+    packages=['nengo_solver_dales'],
+    author='Terry Stewart',
+    url='https://github.com/tcstewar/nengo_solver_dales',
+    )
